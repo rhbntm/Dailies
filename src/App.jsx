8450936ca5d8b7b209d2be67c5ea8dcede0216bd
Ralph Brent Marcelo <ralphbrentm@gmail.com>
2026-08-13 11:08:11 +0800

    chore: initialize daily coding practice

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Day001 from "./challenges/day-001";
+
+function App() {
+  return <Day001 />;
+}
+
+export default App;
---
+++ src/challenges/day-001.jsx
@@ -0,0 +1,10 @@
+function Day001() {
+  return (
+    <main>
+      <h1>Day 001</h1>
+      <p>Challenge goes here.</p>
+    </main>
+  );
+}
+
+export default Day001;
