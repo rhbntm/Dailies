59c68a5fca6debae6eb330e4afb02c4d21eaf835
Ralph Brent Marcelo <ralphbrentm@gmail.com>
2026-08-15 12:40:23 +0800

    feat(session-summary): add timing, criteria, hint, and learning fields
    Day 2: Shopping Cart Item Quantity Adjuster

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import Day001 from "./challenges/day-001";
+// import Day001 from "./challenges/day-001";
+// import Day002 from "./challenges/day-002";
 
 function App() {
-  return <Day001 />;
+  // return <Day001 />;
+  // return <Day002 />
 }
 
 export default App;
---
+++ src/challenges/day-002.jsx
@@ -0,0 +1,58 @@
+/*
+
+import { useState } from "react";
+
+// this is component for the logic for modifying quantity
+function Counter() {
+  let [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count += 1)
+  }
+  function decrement() {
+    setCount(count -= 1)
+  };
+}
+
+// this is the component for rendering the product and quantity
+function ShoppingCartItem(productName, quantity) {
+  let prd = productName
+  let qty = quantity
+  return (
+
+    <div>
+      <button onClick={qty.Counter.increment}>Increment</button>
+      <button onClick={qty.Counter.increment}>Increment</button>
+    </div>
+  )
+}
+
+function Day002() {
+    return (
+      ShoppingCartItem()
+    )
+  }
+export default Day002;
+
+---
+
+==================================================
+REFLECTION
+==================================================
+
+What I learned:
+- My current primary weaknesses: relational ability, judging the importance of each concept/idea/object in relation to one another. How do each concepts connect together.
+
+What I struggled with:
+- I struggled with thinking about the scope of the challenge first and foremost as the root cause which cascaded to all the other problems.
+
+What I figured out without AI:
+- I figured out where I went wrong with my way of approaching this problem and my reasoning.
+
+What I would do differently next time:
+- I said I would do pseudocode more but I didn't do it last time, and so for the next time, it's not pseudocode that I should prioritize, but to actually read the problem and think about the concepts more relationally.
+
+Important insight:
+- It's more about my own meta-thinking that I figured out moreso than the actual technical concepts related to React.
+
+ */
